eaa35058b96d23bcf56b63a47c8ce14b8fcf858d
Sinopsys <kupriyanovkirill@gmail.com>
2017-02-08 16:50:59 +0300

    initial commit of the DNA hw

---
+++ 4/DNA/cmake-build-debug/CMakeFiles/DNA.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DNA.dir/main.cpp.o"
+  "DNA.pdb"
+  "DNA"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DNA.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
