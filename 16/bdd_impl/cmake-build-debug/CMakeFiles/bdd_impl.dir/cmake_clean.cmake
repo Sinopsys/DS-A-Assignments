fe3029a73c242b008fe6d8244fef7c7652ba9b3b
Sinopsys <kupriyanovkirill@gmail.com>
2017-06-06 18:49:03 +0300

    Start BDD task.

---
+++ 16/bdd_impl/cmake-build-debug/CMakeFiles/bdd_impl.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bdd_impl.dir/main.cpp.o"
+  "CMakeFiles/bdd_impl.dir/toyBDD.cpp.o"
+  "bdd_impl.pdb"
+  "bdd_impl"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bdd_impl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
